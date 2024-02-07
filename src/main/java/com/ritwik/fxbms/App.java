eb7ace7456ad0120e100e5812e626ee50327a905
Ritwik_Raj <111105768+ritwik-raj1@users.noreply.github.com>
2024-02-07 07:06:48 +0530

    Create Main Method Class for launching of application without commands

--- src/main/java/com/ritwik/fxbms/App.java
+++ src/main/java/com/ritwik/fxbms/App.java
@@ -10,4 +10,8 @@ public class App extends Application {
     public void start(Stage stage) {
         Model.getInstance().getViewFactory().showLoginWindow();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
